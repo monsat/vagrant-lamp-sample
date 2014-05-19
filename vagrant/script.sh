567b9854de57092d999922ee11d809792395cf21
okuyama junichi <okuyama_junichi@coodip.com>
2014-05-19 19:59:45 +0900

    modify php extensions  add php54-intl

--- vagrant/script.sh
+++ vagrant/script.sh
@@ -27,7 +27,7 @@ yum -y install ntp
 #
 # php
 #
-yum -y install php54 php54-cli php54-pdo php54-mbstring php54-mcrypt php54-pecl-memcache php54-mysql php54-devel php54-common php54-pgsql php54-pear php54-gd php54-xml php54-pecl-xdebug php54-pecl-apc
+yum -y install php54 php54-cli php54-pdo php54-mbstring php54-mcrypt php54-pecl-memcache php54-mysql php54-devel php54-common php54-pgsql php54-pear php54-gd php54-xml php54-pecl-xdebug php54-pecl-apc php54-intl
 touch /var/log/php.log && chmod 666 /var/log/php.log
 cp -a /vagrant/php.ini /etc/php.ini
 
